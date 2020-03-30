cd9385873ee42dce3c527ef20f6f6c2d1d762274
vRimuru-sama <62886531+vRimuru-sama@users.noreply.github.com>
2020-03-31 03:47:09 +0700

    4tools
    Jangan ngamuk yee gw cuman bercanda kok

---
+++ 4tools.py
@@ -0,0 +1,70 @@
+print ("==========welome to welome==========")
+print ("====================================")
+print ("|======autor=VRimuru-sama======|")
+print ("|======Team = Loli team..======|")
+print ("|======jangan luap doa ya======|")
+print ("|======gunakan sebaiknya!======|")
+print ("|======silahkan gunakanya======|")
+print ("====================================")
+print ("====================================")
+{
+  input ("masukan nomor (1) =")
+}
+#output
+{
+ print ("_^_^_^_^_^_^_^_^_^_^_^_^_^_^_^_^_^_")
+}
+{
+ print ("pilih mana ni ada 4 buat ngehack ")
+}
+
+print("""
+=1) hack fb
+=2) hack web
+=3) ddos attack
+=4) ip lacak""")
+
+input ("pilih nomor di atas ya..=")
+{
+  print ("tunggu ya golox proses")
+  }
+
+input ("masukin lgi tod=")
+{
+  print ("tunggu ye sabar")
+}
+input ("lagi masukin tod kgk bisa ini=")
+ 
+{
+  print ("gak bisa tod tulis (1 gitu) =")
+}
+input ("coba lgi=")
+input ("coba lgi=")
+input ("coba lgi=")
+input ("coba lgi=")
+{
+  print ("tekan (enter bro)=")
+}
+while True:
+    print("""
+-ketipu kasian
+-ketipu kasian
+-ketipu kasian
+-ketipu kasian 
+-ketipu kasian
+-ketipu kasian 
+-ketipu kasian
+-ketipu kasian
+-ketipu kasian
+-ketipu kasian 
+-ketipu kasian
+-ketipu kasian 
+-ketipu kasian
+-ketipu kasian
+-ketipu kasian
+-ketipu kasian 
+-ketipu kasian
+-ketipu kasian 
+k-etipu kasian
+-ketipu
+""")
